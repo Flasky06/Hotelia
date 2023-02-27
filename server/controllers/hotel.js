61f896ad2271387bea3d5652748df61dca7c1fc4
Flasky06 <bonnienjuguna106@gmail.com>
2023-02-27 08:56:37 +0300

    client files

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -47,8 +47,14 @@ export const getHotel = async (req, res) => {
 };
 
 export const getHotels = async (req, res, next) => {
+	const { min, max, ...others } = req.query;
+
 	try {
-		const hotels = await Hotel.find(req.query);
+		const hotels = await Hotel.find({
+			...others,
+			cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+		}).limit(req.query.limit);
+
 		res.status(200).json(hotels);
 	} catch (err) {
 		next(err);
